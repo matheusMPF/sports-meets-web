418ddda409e73a2ec364a4fe373d83852b4ff315
Matheus <matheus.mpf18@gmail.com>
2024-05-14 10:00:07 -0300

    Criando tela de Login

--- src/pages/Signup.tsx
+++ src/pages/Signup.tsx
@@ -1,7 +1,15 @@
+import { ButtonSm } from "../components/ui/ButtonSm"
+import { BaseInput } from "../components/ui/Input"
+
 export const Signup = () => {
     return (
-        <section>
-            Tela de "Signup"
+        <section className="w-screen h-screen flex items-center justify-center" >
+            <form action="" className="bg-background-200 p-16  rounded-lg flex flex-col gap-4">
+                <BaseInput placeholder="E-mail" />
+                <BaseInput type="password" />
+                <ButtonSm>Cadastrar</ButtonSm>
+            </form>
+
         </section>
     )
 }
