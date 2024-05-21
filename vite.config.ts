e3e62a1c980cf02a3b76fd39d390be3885da1b13
Matheus <matheus.mpf18@gmail.com>
2024-05-20 22:08:17 -0300

    Testing again create deploy application

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import react from '@vitejs/plugin-react';
 export default defineConfig({
   base: '/sports-meets-web/',
   build: {
-    outDir: 'docs'
+    outDir: '/(root)'
   },
   plugins: [react()],
 });
