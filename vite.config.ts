01f76fe1a314adfbc01821f4f579887d9f72f832
Matheus <matheus.mpf18@gmail.com>
2024-05-20 23:01:51 -0300

    Trocando rota base do sistema

--- vite.config.ts
+++ vite.config.ts
@@ -2,7 +2,7 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
-  base: '/sports-meets-web/',
+  base: '/',
   build: {
     outDir: 'dist'
   },
